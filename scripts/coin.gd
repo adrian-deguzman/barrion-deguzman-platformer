c12b4d4969a7fc007407e1f54363debd883d3832
Adrian De Guzman <adrian.deguzman@student.ateneo.edu>
2026-03-09 07:33:02 +0800

    added: disappearing coins

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,10 +1,23 @@
 extends Area2D
 
+# Grab references to our child nodes
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var collision_shape = $CollisionShape2D
+
 func _on_body_entered(body: Node2D) -> void:
-	# 1. This prints exactly what touched the coin to the Output console
-	print("Something touched the coin! It was: ", body.name)
-	
-	# 2. Check the group
+	if not body:
+		return
+		
+	# Check if it's the player
 	if body.is_in_group("Player"):
-		print("It is the player! Deleting coin...")
+		# 1. Safely turn off the hitbox so it can't be collected twice
+		collision_shape.set_deferred("disabled", true)
+		
+		# 2. Play the new animation
+		animated_sprite.play("disappear")
+		
+		# 3. Pause this specific function until the animation is completely done
+		await animated_sprite.animation_finished
+		
+		# 4. Now safely delete the strawberry
 		queue_free()
